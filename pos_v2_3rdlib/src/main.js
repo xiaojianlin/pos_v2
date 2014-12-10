945494961d9f7cb912a5e69b231be3e055e0717c
xxiao <843814555@qq.com>
2014-12-09 19:18:18 -0500

    make getCartItems

--- pos_v2_3rdlib/src/main.js
+++ pos_v2_3rdlib/src/main.js
@@ -1,19 +1,32 @@
 function printInventory(inputs){
-  var carItems = getCarItems(inputs);
-  var inventoryText = getInventoryText(carItems);
+  var cartItems = getCartItems(inputs);
+  var inventoryText = getInventoryText(cartItems);
   console.log(inventoryText);
 }
 
 
-function getCarItems(inputs){
-  var carItems = '123' ;
-
-  console.log(carItems);
-  return carItems;
+function getCartItems(inputs){
+  var cartItems = [];
+  _.forEach(inputs,function(inputs){
+    var input = inputs.split('-');
+    var barcode = input[0];
+    var count = 1 ;
+    if(input[1]){
+      count = parseFloat(input[1]);
+    }
+  var item =_.find(loadAllItems(),{barcode : barcode});
+  var cartItem=_.find(cartItems,function(cartItem){
+    return barcode === cartItem.item.barcode;
+  });
+  cartItem ? cartItem.count += count :
+            cartItems.push({ item: item , count: count });
+  });
+  console.log(cartItems);
+  return cartItems;
 }
 
 
-function getInventoryText(carItems){
+function getInventoryText(cartItems){
   var inventoryText = 'test2';
   console.log(inventoryText);
   return inventoryText;
