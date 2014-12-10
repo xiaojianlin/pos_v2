cf2b6ba00647813c819fa6e50d5cf40c6ac893ee
xxiao <843814555@qq.com>
2014-12-09 19:00:30 -0500

    make master function

--- pos_v2_3rdlib/src/main.js
+++ pos_v2_3rdlib/src/main.js
@@ -1,4 +1,20 @@
 function printInventory(inputs){
-  var cartItems = 'test';
-  console.log(cartItems);
+  var carItems = getCarItems(inputs);
+  var inventoryText = getInventoryText(carItems);
+  console.log(inventoryText);
+}
+
+
+function getCarItems(inputs){
+  var carItems = '123' ;
+
+  console.log(carItems);
+  return carItems;
+}
+
+
+function getInventoryText(carItems){
+  var inventoryText = 'test2';
+  console.log(inventoryText);
+  return inventoryText;
 }
